fd079f94af5efcb4b71bc99a579fe134fb1bba1e
Денис Тимофеев <maksimka.timofeev.2002@list.ru>
2020-04-13 13:01:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,50 @@
+﻿#include <math.h> 
+#include<iostream> 
+using namespace std;
+
+float F1(float x)
+{
+	float result = 0;
+
+	if (x >= 0)
+	{
+		result = 5 * x + 7;
+	}
+	else
+	{
+		result = 1 / x;
+	}
+	return result;
+}
+float F2(float& x)
+
+{
+	x = F1(x);
+	return x;
+
+}
+
+int main()
+{
+	int m, n;
+	cout << "Vvedite nachalo = ";
+	cin >> m;
+	cout << "Vvedite konech = ";
+	cin >> n;
+	cout << "Function 1:" << endl;
+	for (float i = m; i < n; i++)
+	{
+		cout << "i=" << i << ";F1(i)=" << F1(i) << endl;
+	}
+	cout << "Function 2:" << endl;
+
+	for (float i = m; i < n; i++)
+
+	{
+
+		cout << " i = " << i << "; F2(i) = " << F2(i) << endl;
+
+	}
+	system("pause");
+	return 0;
+}
