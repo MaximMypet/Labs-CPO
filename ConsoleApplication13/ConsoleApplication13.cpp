681095c097e515e801bc6218b97f015bb1d2560d
Денис Тимофеев <maksimka.timofeev.2002@list.ru>
2020-04-13 13:24:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+void F1(double* mas, int N) {
+	setlocale(LC_ALL, "RUS");
+	int a;
+	double x;
+	for (int i = 0; i < N; i++) {
+		a = (int)mas[i];
+		x = mas[i] - a;
+		cout << "цел" << a << endl;
+		cout << "дроб" << x << endl;
+	}
+}
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int N;
+	cin >> N;
+	double* mas = new double[N];
+	for (int i = 0; i < N; i++) {
+		cin >> mas[i];
+	}
+	F1(mas, N);
+	system("pause");
+	return 0;
+}
